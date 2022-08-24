4a4b3b21afbfcae0f180f377ab6e4d6a791cdade
Nathan Mills <rivernate@gmail.com>
2022-08-24 16:33:21 -0600

    Only specify languages I care about in Tree Sitter (#48)

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -1,9 +1,12 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "all",
-    sync_install = false,
-
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { "bash", "c", "c_sharp", "clojure", "commonlisp", "cpp",
+    "css", "dockerfile", "gitignore", "go", "haskell", "help", "html", "java",
+    "javascript", "json", "lua", "make", "markdown", "proto", "python", "ruby",
+    "rust", "scala", "toml", "tsx", "typescript", "vim", "yaml"
+  },
+  sync_install = false,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
 }
