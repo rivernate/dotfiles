0a38c95b861282fe45425df1eb8709e316c00e0c
Nathan Mills <rivernate@gmail.com>
2022-08-30 18:20:52 -0600

    setup rust-analyzer

--- dot_config/nvim/lua/rivernate/lsp.lua
+++ dot_config/nvim/lua/rivernate/lsp.lua
@@ -81,3 +81,11 @@ require("lspconfig")["sumneko_lua"].setup {
     }
   },
 }
+require('lspconfig')['rust_analyzer'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    -- Server-specific settings...
+    settings = {
+      ["rust-analyzer"] = {}
+    }
+}
