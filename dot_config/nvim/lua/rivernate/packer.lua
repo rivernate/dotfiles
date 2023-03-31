e5c203f1da9be1f9cfe6ef5150e9b9d6c23d7443
Nathan Mills <rivernate@gmail.com>
2023-03-31 10:04:54 -0600

    remove neogit

--- dot_config/nvim/lua/rivernate/packer.lua
+++ dot_config/nvim/lua/rivernate/packer.lua
@@ -33,7 +33,6 @@ return require("packer").startup(function()
   use("simrat39/symbols-outline.nvim")
   use("github/copilot.vim")
 
-  use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
   use({ "bazelbuild/vim-bazel", requires = { { "google/vim-maktaba" } } })
   use({
     "williamboman/mason-lspconfig.nvim",
