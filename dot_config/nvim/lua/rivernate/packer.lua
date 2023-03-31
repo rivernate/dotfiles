85b42fd9f205cfe2749d9f38edb5bfb0c9b941b9
Nathan Mills <rivernate@gmail.com>
2023-03-31 09:38:48 -0600

    setup gh-octo

--- dot_config/nvim/lua/rivernate/packer.lua
+++ dot_config/nvim/lua/rivernate/packer.lua
@@ -1,13 +1,17 @@
 local fn = vim.fn
-local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+  packer_bootstrap = fn.system({
+    "git", "clone", "--depth", "1",
+    "https://github.com/wbthomason/packer.nvim", install_path
+  })
 
   -- Ref: https://github.com/wbthomason/packer.nvim/issues/739#issuecomment-1019280631
-  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+  vim.o.runtimepath = vim.fn.stdpath("data") .. "/site/pack/*/start/*," ..
+      vim.o.runtimepath
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 vim.cmd([[
   augroup packer_user_config
@@ -17,58 +21,62 @@ vim.cmd([[
 ]])
 
 return require("packer").startup(function()
-  use "wbthomason/packer.nvim"
+  use("wbthomason/packer.nvim")
 
   -- auto complete
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/nvim-cmp"
-  use "L3MON4D3/LuaSnip"
-  use "saadparwaiz1/cmp_luasnip"
-  use "onsails/lspkind-nvim"
-  use "simrat39/symbols-outline.nvim"
-  use "github/copilot.vim"
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/nvim-cmp")
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip")
+  use("onsails/lspkind-nvim")
+  use("simrat39/symbols-outline.nvim")
+  use("github/copilot.vim")
 
-  use {
-    "TimUntersberger/neogit",
-    requires = 'nvim-lua/plenary.nvim'
-  }
-  use {
-    "bazelbuild/vim-bazel",
-    requires = { {"google/vim-maktaba"} }
-  }
-  use { "williamboman/mason-lspconfig.nvim",
-        requires = {
-          {'neovim/nvim-lspconfig'}, -- Configurations for Nvim LSP
-          {"williamboman/mason.nvim"}
-        }
-  }
-  use "williamboman/mason.nvim"
-  use "jose-elias-alvarez/null-ls.nvim"
-  use { "jay-babu/mason-null-ls.nvim",
-        requires = {
-          {"williamboman/mason.nvim"},
-          {"jose-elias-alvarez/null-ls.nvim"}
-        }
-  }
-  use 'nvim-tree/nvim-web-devicons'
-  use "nvim-treesitter/nvim-treesitter"
-  use "nvim-treesitter/nvim-treesitter-context"
-  use {
+  use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
+  use({ "bazelbuild/vim-bazel", requires = { { "google/vim-maktaba" } } })
+  use({
+    "williamboman/mason-lspconfig.nvim",
+    requires = {
+      { "neovim/nvim-lspconfig" },     -- Configurations for Nvim LSP
+      { "williamboman/mason.nvim" }
+    }
+  })
+  use("williamboman/mason.nvim")
+  use("jose-elias-alvarez/null-ls.nvim")
+  use({
+    "jay-babu/mason-null-ls.nvim",
+    requires = {
+      { "williamboman/mason.nvim" }, { "jose-elias-alvarez/null-ls.nvim" }
+    }
+  })
+  use("nvim-tree/nvim-web-devicons")
+  use("nvim-treesitter/nvim-treesitter")
+  use("nvim-treesitter/nvim-treesitter-context")
+  use({
     "nvim-telescope/telescope.nvim",
-    requires =  { {"nvim-lua/plenary.nvim" } }
-  }
-  use "prettier/vim-prettier"
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-  use 'mfussenegger/nvim-jdtls'
-  use "b0o/schemastore.nvim"
+    requires = { { "nvim-lua/plenary.nvim" } }
+  })
+  use("prettier/vim-prettier")
+  use("neovim/nvim-lspconfig")   -- Configurations for Nvim LSP
+  use("mfussenegger/nvim-jdtls")
+  use("b0o/schemastore.nvim")
 
-  --THEMES--
-  use "rktjmp/lush.nvim"
+  use({
+    "pwntester/octo.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim",
+      "kyazdani42/nvim-web-devicons"
+    },
+    config = function() require("octo").setup() end
+  })
 
-  use "gruvbox-community/gruvbox"
-  use "Scysta/pink-panic.nvim"
-  use 'folke/tokyonight.nvim'
-  use 'haishanh/night-owl.vim'
-  --END THEMES--
+  -- THEMES--
+  use("rktjmp/lush.nvim")
+
+  use("gruvbox-community/gruvbox")
+  use("Scysta/pink-panic.nvim")
+  use("folke/tokyonight.nvim")
+  use("haishanh/night-owl.vim")
+  -- END THEMES--
 end)
