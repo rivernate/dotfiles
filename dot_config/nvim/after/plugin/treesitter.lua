a5d20cb9cc345f84005f50753e158b94f63084d3
Nathan Mills <rivernate@gmail.com>
2023-05-07 22:22:07 -0400

    Update TS help to vimdoc

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -1,8 +1,8 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "bash", "c", "c_sharp", "clojure", "commonlisp", "cpp",
-    "css", "dockerfile", "gitignore", "go", "haskell", "help", "html", "java",
+    "css", "dockerfile", "gitignore", "go", "haskell",  "html", "java",
     "javascript", "json", "lua", "make", "markdown", "proto", "python", "ruby",
-    "rust", "scala", "toml", "tsx", "typescript", "vim", "yaml"
+    "rust", "scala", "toml", "tsx", "typescript", "vim", "vimdoc", "yaml"
   },
   sync_install = false,
   highlight = {
