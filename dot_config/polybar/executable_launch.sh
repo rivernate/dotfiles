a5a4a25f42f8ef7ddb0b5b795f254e036cbe64d5
Nathan Mills <rivernate@gmail.com>
2024-07-17 11:19:34 -0600

    Add better support to detect and use external monitors (#158)

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 export FC_DEBUG=0
 export PRIMARY_MONITOR=$(xrandr -q | grep primary | awk '{print $1;}')
-if [ $PRIMARY_MONITOR == "eDP1" ] || [ $PRIMARY_MONITOR == "eDP" ]
-then
-  echo Laptop primary
+
+# Terminate already running polybar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+if [[ $PRIMARY_MONITOR == "eDP" || $PRIMARY_MONITOR == "eDP1" ]]; then
+  echo "Laptop primary"
 else
-  echo Laptop not Primary
+  echo "Laptop not primary"
   polybar laptop &
 fi
+
 echo $PRIMARY_MONITOR
 polybar main &
 feh --bg-center --randomize ~/.wallpaper/*
